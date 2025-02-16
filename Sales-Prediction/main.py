2b449c251f873d87ff69017d8347759023bd4490
Robert Freitas <robertfreitas@westernwyoming.edu>
2025-02-16 04:39:32 +0000

    Ready for grading.

--- Sales-Prediction/main.py
+++ Sales-Prediction/main.py
@@ -1,19 +1,19 @@
 #
 # Robert Freitas
-# 02/12/2025
+# 02/15/2025
 # Sales Prediction Programming Project
 # COSC 1010
 #
 
 # Variables to hold the sales total and the profit
-sales_total = 533.00
-profit = 0.00
+total_sales = 0.0
+PROFIT = 0.23
 
 # Get the amount of projected sales.
-sale_total = float(input('Enter the projected sales:'))
+total_sales = float(input('Enter the projected sales:'))
 
 # Calculate the projected profit.
-profit = sales_total * 0.23
+profit = total_sales * 0.23
 
 # Print the projected profit.
 print('The profit is $', format(profit, ',.2f'))
