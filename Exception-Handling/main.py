a226f71b27241bb50ff6fc4a4cfe44f50e027c16
Robert Freitas <robertfreitas@westernwyoming.edu>
2025-04-01 18:50:29 -0500

    Update main.py

--- Exception-Handling/main.py
+++ Exception-Handling/main.py
@@ -5,3 +5,28 @@
 # COSC 1010
 #
 # Use comments liberally throughout the program. 
+
+# Calculate the average of a list of numbers
+def main():
+    try:
+        numbersFile = open( "numbers.txt", "r" )
+
+# Declare Variables 
+        total = 0
+        numberOfLines = 0
+        line = numbersFile.readline()
+
+# Implement while loop to move between lines in file.
+        while line != "":
+            numberOfLines += 1
+            total += int( line )
+            line = numbersFile.readline()
+# Calculate the average.
+except
+    average = total / numberOfLines
+
+# Use print function to show average.
+    print( "The average is", average )
+
+# Call main function.
+main()   
